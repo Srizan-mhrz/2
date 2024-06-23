41af662f1c6f481b63d731ac6a340784464c3714
Srizan-mhrz <shrijan.mhrz@gmail.com>
2024-06-23 10:08:12 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,106 @@
+# x=6
+# y=3
+# if x>y:
+#     print("x is greater than y")
+# elif x==y:
+#     print("x is equal to y")
+# else: 
+#     print("y is greater than x")    
+
+# a=4
+# b=5
+# c=6
+# if a>b and a>c:
+#     print("a is greater number")
+# elif b>c and b>a:
+#         print("b is greater number")
+# else:
+#             print("c is greater number")
+
+# num1=int(input("enter a number:"))
+# if num1%2==0:
+#     print("the numbe is even")
+# else:   
+#  print("the number is odd") 
+
+# num1=int(input("enter a number:"))
+# if num1%3==0:
+#     print("the number is divisible by 3")
+# elif num1%5==0:
+#     print("the number is divisible by 5")
+# else:
+#     print("the number is not divisible")     
+
+# age=int(input("enetr your age:"))
+# if age>=18 and age<=40:
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible to vote")  
+
+# n1=int(input("enter a marks for math:"))
+# n2=int(input("enter a marks for english:"))
+# n3=int(input("enter a marks for physics:"))
+# n4=int(input("enter a marks for chemistry:"))
+# n5=int(input("enter a marks for nepali:"))
+# tot=((n1+n2+n3+n4+n5)/500)*100
+# print(tot)
+# if tot>=35 and tot<45:
+#     print("you obtained C grade")
+# elif tot>=45 and tot<60:
+#     print("you obtained B grade")
+# elif tot>=60 and tot<75:  
+#     print("you obtained A grade")
+# elif tot>=75 and tot<=100:
+#     print("you obtained A+ grade") 
+# else:
+#     print("you failed")            
+
+# print("1.add")
+# print("2.sub")
+# print("3.mul")
+# print("4.div")
+# print("5.mod")
+# choice= int(input("enter your choice"))
+# if choice==1:
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a+b)
+# elif choice==2:
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a-b)
+# elif choice==3:
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a*b)
+# elif choice==4:
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a/b)
+# else choice==5:
+# a=int(input("enter a number:"))
+# b=int(input("enter another number:"))
+# print(a%b)
+
+username=input("enter your username:")
+password=input("enter your password:")
+if username=='shrijan':
+ if password=='shrijan123$#':
+    print("welcome user")
+ else:
+    print("invalid password")
+else:
+    print("invalid username")    
+
+
+   
+    
+
+
+   
+    
+
+   
+    
+
+
