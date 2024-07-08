791755bfa234d8bb8b325b57807b4245b348664c
Srizan-mhrz <shrijan.mhrz@gmail.com>
2024-07-08 09:39:30 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -1,180 +1,58 @@
-# x=6
-# y=3
-# if x>y:
-#     print("x is greater than y")
-# elif x==y:
-#     print("x is equal to y")
-# else: 
-#     print("y is greater than x")    
+import os
+if not os.path.exists('user.txt'):
+    handle=open('user.txt','w')
+    handle.close()
+books=[
+    {'id':1,'name':'python','price':100},
+    {'id':2,'name':'java','price':200},
+    {'id':3,'name':'c++','price':300},
+    {'id':4,'name':'c#','price':400},
+    {'id':5,'name':'php','price':500}
+]
+def register():
+    print("=========register=========")
+    username=input("enter username:").strip()
+    if username in open('user.txt').read():
+        print("username already exists")
+        exit()
+    password=input("enter the password").strip()
+    confirm_password=input("enter the password again").strip()
+    if password!=confirm_password:
+        print("password didnt match")
+        exit()
+    handle=open('user.txt','a')
+    handle.write(f'{username},{password}\n')
+    handle.close()
+    print("user resgistered succesfully")
 
-# a=4
-# b=5
-# c=6
-# if a>b and a>c:
-#     print("a is greater number")
-# elif b>c and b>a:
-#         print("b is greater number")
-# else:
-#             print("c is greater number")
+def login():
+    print("=======login========")
+    username=input("enter username:").strip()
+    password=input("enter password:").strip()
+    users=open('user.txt').readlines()
+    is_login=False
+    for user in users:
+        user=user.strip().split(',')
+        if user[0]==username and user[1]==password:
+            is_login=True
+        if is_login:
+            print(f"=======welcome:{username}========")
+            print("ID\t NAME\t PRICE")
+            for book in books:
+                print(f'{book['id']}\t{book['name']}\t{book['price']}')
+                
+            else:
+                print("invalid username or password")
+                exit()
 
-# num1=int(input("enter a number:"))
-# if num1%2==0:
-#     print("the numbe is even")
-# else:   
-#  print("the number is odd") 
-
-# num1=int(input("enter a number:"))
-# if num1%3==0:
-#     print("the number is divisible by 3")
-# elif num1%5==0:
-#     print("the number is divisible by 5")
-# else:
-#     print("the number is not divisible")     
-
-# age=int(input("enetr your age:"))
-# if age>=18 and age<=40:
-#     print("you are eligible to vote")
-# else:
-#     print("you are not eligible to vote")  
-
-# n1=int(input("enter a marks for math:"))
-# n2=int(input("enter a marks for english:"))
-# n3=int(input("enter a marks for physics:"))
-# n4=int(input("enter a marks for chemistry:"))
-# n5=int(input("enter a marks for nepali:"))
-# tot=((n1+n2+n3+n4+n5)/500)*100
-# print(tot)
-# if tot>=35 and tot<45:
-#     print("you obtained C grade")
-# elif tot>=45 and tot<60:
-#     print("you obtained B grade")
-# elif tot>=60 and tot<75:  
-#     print("you obtained A grade")
-# elif tot>=75 and tot<=100:
-#     print("you obtained A+ grade") 
-# else:
-#     print("you failed")            
-
-# print("1.add")
-# print("2.sub")
-# print("3.mul")
-# print("4.div")
-# print("5.mod")
-# choice= int(input("enter your choice"))
-# if choice==1:
-# a=int(input("enter a number:"))
-# b=int(input("enter another number:"))
-# print(a+b)
-# elif choice==2:
-# a=int(input("enter a number:"))
-# b=int(input("enter another number:"))
-# print(a-b)
-# elif choice==3:
-# a=int(input("enter a number:"))
-# b=int(input("enter another number:"))
-# print(a*b)
-# elif choice==4:
-# a=int(input("enter a number:"))
-# b=int(input("enter another number:"))
-# print(a/b)
-# else choice==5:
-# a=int(input("enter a number:"))
-# b=int(input("enter another number:"))
-# print(a%b)
-
-# username=input("enter your username:")
-# password=input("enter your password:")
-# if username=='shrijan':
-#  if password=='shrijan123$#':
-#     print("welcome user")
-#  else:
-#     print("invalid password")
-# else:
-#     print("invalid username")  
-
-# print("=============welcome to mero atm=================")  
-# pin=int(input("please enter the pin:"))  
-# if pin==1234:
-#     amount=100000
-# print("1.withdraw")
-# print("2.balance enquiry")
-# option=int(input("what do you want to do:"))
-# if option==1:
-#    new_amount=int(input("enter the amount to withdraw"))
-#    if amount>new_amount:
-#       rem=amount-new_amount
-#       print("remaining balance:",rem)
-#       print("withdrawn amount:",new_amount)
-#    else:
-#       print("insufficient balance")
-# elif option==2:
-#    print("your total amount is:",amount)
-         
-
-# else:
-#    print("incorrect passsword")
-#    exit()
-
-# print("welcome to my shop") 
-# print("1.dell laptop")
-# print("2.hp laptop")
-# print("3.mac")
-# option=int(input("what do you want to buy:"))
-# if option==1:
-#    stock=10
-#    price=1000000
-#    quantity=int(input("how many do you want:"))
-#    if quantity<=stock:
-#       tot=price*quantity
-#       print("your total amount is:",tot)
-#    else:
-#          print("sorry out of stock")
-#          exit()
-# elif option==2:
-#    stock=9
-#    price=200000
-#    quantity=int(input("how many do you want:"))
-#    if quantity<=stock:
-#       tot=price*quantity
-#       print("your total amount is:",tot)
-#    else:
-#          print("sorry out of stock")
-#          exit()
-# else:
-#    stock=6
-#    price=300000
-#    quantity=int(input("how many do you want:"))
-#    if quantity<=stock:
-#       tot=price*quantity
-#       print("your total amount is:",tot)
-#    else:
-#          print("sorry out of stock")
-#          exit()   
-
-# students_list=['ramu','hari','belove']
-# print("1.add student")
-# print("2.remove student")
-# print("3.display student")
-# print("4.exit")
-# print=input("enter the student")
-# if option==1:
-
-
-    
-
-               
+question=input('do you have any account?(y/n):').strip()
+if question=='y':
+    login()
+else:
+    register()    
 
 
 
 
-   
-    
-
-
-   
-    
-
-   
-    
 
 
